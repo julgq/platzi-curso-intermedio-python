0cc4206f7e69b4b50e516af8c6fabdc79c59d580
Julio Gtz <jc.gutierrezq@gmail.com>
2021-09-19 11:41:55 -0600

    list comprenhensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -15,6 +15,14 @@ def run():
 	# solución con list comprehensions, la misma solución pero con list comprenhensions:
 	squares_comprenhension = [i**2 for i in range(1, 101) if i % 3 != 0]
 	print(squares_comprenhension)
+	print('\n')
+
+	# RETO: Crear un list comprehension, 
+	# una lista de todos los múltiplos de 4 que la vez también  
+	# son múltiplos de 6 y de 9, hasta 5 dígitos.
+
+	squares_reto = [i for i in range(1, 501) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
+	print(squares_reto)
 
 
 if __name__ == '__main__':
